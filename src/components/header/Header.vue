0d5c7b3a89da775de55a572cd14a16a33661f7fd
Nguyen Tan Khanh <khanhdpdx@gmail.com>
2022-12-15 01:19:33 +0700

    Change header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -12,35 +12,73 @@
     <SearchInput />
     <!-- menu -->
     <div class="flex gap-x-8 mx-auto" v-if="getLoggedIn">
-      <router-link :to="{ path: '/test' }" tag="span"
-        class="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium hover:bg-gray-100">Khám phá</router-link>
-      <router-link :to="{ path: '/shop' }" tag="span"
-        class="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium hover:bg-gray-100">Quản lý shop</router-link>
-      <router-link :to="{ path: '/' }" tag="span"
-        class="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium hover:bg-gray-100">Đơn hàng</router-link>
+      <router-link
+        :to="{ path: '/test' }"
+        tag="span"
+        class="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium hover:bg-gray-100"
+        >Khám phá</router-link
+      >
+      <router-link
+        :to="{ path: '/shop' }"
+        tag="span"
+        class="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium hover:bg-gray-100"
+        >Quản lý shop</router-link
+      >
+      <router-link
+        :to="{ path: '/' }"
+        tag="span"
+        class="cursor-pointer rounded-sm py-1 px-2 text-sm font-medium hover:bg-gray-100"
+        >Đơn hàng</router-link
+      >
     </div>
     <div v-else>
-      <button class="btn btn-active btn-secondary mr-4" @click.prevent="redirectRegisterSeller()">Are you a seller?
-        Register right now here!</button>
-      <button class="btn btn-sm btn-primary mr-4" @click.prevent="redirectLogin()">Login</button>
-      <button class="btn btn-sm btn-accent" @click.prevent="redirectRegisterUser()">Register</button>
+      <button
+        class="btn btn-active btn-secondary mr-4"
+        @click.prevent="redirectRegisterSeller"
+      >
+        Are you a seller? Register right now here!
+      </button>
+      <button
+        class="btn btn-sm btn-primary mr-4"
+        @click.prevent="redirectLogin"
+      >
+        Login
+      </button>
+      <button
+        class="btn btn-sm btn-accent"
+        @click.prevent="redirectRegisterUser"
+      >
+        Register
+      </button>
     </div>
 
     <div class="flex-none" v-if="getLoggedIn">
       <div class="dropdown dropdown-end">
         <label tabindex="0" class="btn btn-ghost btn-circle">
           <div class="indicator">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
             </svg>
             <span class="badge badge-sm indicator-item">{{
               cartItemSize
             }}</span>
           </div>
         </label>
-        <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+        <div
+          tabindex="0"
+          class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+        >
           <div class="card-body">
             <span class="font-bold text-lg">{{ cartItemSize }} Items</span>
             <div class="card-actions">
@@ -61,7 +99,10 @@
             <img src="https://placeimg.com/80/80/people" />
           </div>
         </label>
-        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <ul
+          tabindex="0"
+          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+        >
           <li>
             <a class="justify-between">
               Profile
@@ -69,7 +110,7 @@
             </a>
           </li>
           <li><a>Settings</a></li>
-          <li id="logout" @click.prevent="logout()"><a>Logout</a></li>
+          <li id="logout" @click.prevent="logout"><a>Logout</a></li>
         </ul>
       </div>
     </div>
@@ -104,7 +145,6 @@ export default {
         this.cartItemSize = response.cartItemDtoList.length;
       });
     },
-  },
     redirectLogin() {
       this.$router.push("/login");
     },
@@ -115,23 +155,21 @@ export default {
       this.$router.push("/registerSeller");
     },
     logout() {
-      this.$store
-        .dispatch("auth/logout")
-        .then(
-          () => {
-            this.$router.push("/login");
-          },
-          (error) => {
-            this.loading = false;
-            this.message =
-              (error.response &&
-                error.response.data &&
-                error.response.data.message) ||
-              error.message ||
-              error.toString();
-          }
-        );
-    }
-  }
+      this.$store.dispatch("auth/logout").then(
+        () => {
+          this.$router.push("/login");
+        },
+        (error) => {
+          this.loading = false;
+          this.message =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+        }
+      );
+    },
+  },
 };
 </script>
